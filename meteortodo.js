672a079cb3cdd4b2ad12c29720910e9bed8dc7ae
cdelauder <cdelauder@gmail.com>
2014-08-14 16:21:01 -0700

    i am missing something somewhere and getting strange errors. I dont think i will make mch more progress on this today. the server side routing is more complicqted than client side routing. I suspect it has to do with what is being passed in my response.end function.

--- meteortodo.js
+++ meteortodo.js
@@ -1,13 +1,41 @@
 Todos = new Meteor.Collection('todos');
+var counter = 0
 
-Session.setDefault('list_id', null);
-Session.setDefault('editing_listname', null);
-Session.setDefault('tag_filter', null);
-Session.setDefault('editing_itemname', null);
-Session.setDefault('editing_addtag', null);
+Router.map(function () {
+  this.route('serverFile', {
+    where: 'server',
+    path: '/',
 
-Router.map(function() {
-  this.route('/', 
-    {path:'/',
-     })
-})
+    action: function () {
+      var filename = this.params.filename;
+
+      this.response.writeHead(200, {'Content-Type': 'JSON'});
+      this.response.end(EJSON.stringify(getAll()));
+    }
+  });
+  this.route('serverFile', {
+    where: 'server',
+    path: '/',
+    method: 'POST',
+
+    action: function () {
+      var filename = this.params.filename;
+
+      this.response.writeHead(200, {'Content-Type': 'JSON'});
+      this.response.end(create());
+    }
+  });
+});
+
+function getAll() {
+  return Todos.find({})
+}
+
+function create() {
+  Todos.insert({
+    title: title,
+    time: Date.now(),
+    id: counter,
+  })
+  counter++
+}
